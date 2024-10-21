bfe9e74bf853f3dceba8b8cbd8155ccfba303d78
E-specter <especter.o.a@gmail.com>
2024-10-21 13:52:28 -0500

    Carga inicial

---
+++ src/main/java/dev/Lovelace/EventPro/EventPro.java
@@ -0,0 +1,14 @@
+package dev.Lovelace.EventPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventPro {
+  
+  public static void main(String[] args) {
+    SpringApplication.run(EventPro.class, args);
+    
+  }
+
+}
---
+++ src/main/java/dev/Lovelace/EventPro/modelo/NewInterface.java
@@ -0,0 +1,5 @@
+package dev.Lovelace.EventPro.modelo;
+
+public interface NewInterface {
+  
+}
